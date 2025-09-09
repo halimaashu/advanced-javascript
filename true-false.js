90644b62330c3aa0455000c24a9a9f3bf4e29516
md ashik mia <halima520ashu@gmail.com>
2025-09-09 22:46:51 +0600

    everything is changed

--- true-false.js
+++ true-false.js
@@ -1,1 +1,9 @@
-const 
+//falsy:NoN,"",0, false,undefind,null,
+//truely:[]," ","0"
+let nam = 0;
+if(nam){
+    console.log('your condition is true')
+}
+else{
+    console.log('your condition is false')
+}
